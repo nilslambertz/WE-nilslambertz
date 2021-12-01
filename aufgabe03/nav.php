f718547a86f173b989d65ed9998a229dbf2aa46a
nilslambertz <nilslambertz@web.de>
2021-12-01 14:16:02 +0100

    started aufgabe03, changed files to php, added nav.php

---
+++ aufgabe03/nav.php
@@ -0,0 +1,22 @@
+<div class="col-2">
+    <ul class="list-group">
+        <li class="list-group-item">
+            <a href="login.php">Login</a>
+        </li>
+        <li class="list-group-item">
+            <a href="projekte.php">Projekte</a>
+        </li>
+        <li class="list-group-item">
+            <a href="todos.php">Aktuelles Projekt</a>
+        </li>
+        <li class="list-group-item ml-4">
+            <a href="reiter.php">Reiter</a>
+        </li>
+        <li class="list-group-item ml-4">
+            <a href="aufgaben.php">Aufgaben</a>
+        </li>
+        <li class="list-group-item ml-4">
+            <a href="mitglieder.php">Mitglieder</a>
+        </li>
+    </ul>
+</div>
---
+++ aufgabe03/reiter.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Reiter</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="jumbotron text-center">
+            <h1>Aufgabenplaner: Reiter</h1>
+        </div>
+        <div class="row">
+            <?php
+                include("nav.php");
+            ?>
+            <div class="col">
+                <table class="table">
+                    <thead class="thead-light">
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Beschreibung</th>
+                        <th scope="col"></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>ToDo</td>
+                        <td>Dinge, die erledigt werden müssen.</td>
+                        <td class="text-right">
+                            <object height="20" data="icons/edit-box.svg" type="image/svg+xml">Bearbeiten</object>
+                            <object height="20" data="icons/trash-bin.svg" type="image/svg+xml">Löschen</object>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Erledigt</td>
+                        <td>Dinge, die erledigt sind.</td>
+                        <td class="text-right">
+                            <object height="20" data="icons/edit-box.svg" type="image/svg+xml">Bearbeiten</object>
+                            <object height="20" data="icons/trash-bin.svg" type="image/svg+xml">Löschen</object>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Verschoben</td>
+                        <td>Dinge, die später erledigt werden.</td>
+                        <td class="text-right">
+                            <object height="20" data="icons/edit-box.svg" type="image/svg+xml">Bearbeiten</object>
+                            <object height="20" data="icons/trash-bin.svg" type="image/svg+xml">Löschen</object>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                <form>
+                    <h3>Bearbeiten/Erstellen:</h3>
+                    <div class="form-group">
+                        <label for="reiterBezeichnung">Bezeichnung des Reiters:</label>
+                        <input type="text" id="reiterBezeichnung" placeholder="Reiter" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label for="reiterBeschreibung">Beschreibung:</label>
+                        <textarea type="text" id="reiterBeschreibung" placeholder="Beschreibung" class="form-control"></textarea>
+                    </div>
+                    <button type="button" class="btn btn-primary">Speichern</button>
+                    <button type="button" class="btn btn-info">Reset</button>
+                </form>
+            </div>
+            <div class="col-2"></div>
+        </div>
+    </div>
+</body>
+</html>
