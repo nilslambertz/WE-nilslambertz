740889b90b42747bef5097200b0093ee8dc37898
nilslambertz <nilslambertz@web.de>
2022-01-06 01:24:22 +0100

    remove extra text in login until it's needed

--- codeigniter/app/Views/login.php
+++ codeigniter/app/Views/login.php
@@ -18,13 +18,7 @@
                 <label for="password">Passwort:</label>
                 <input type="password" id="password" name="password" placeholder="Passwort" class="form-control"/>
             </div>
-            <div class="form-group form-check">
-                <input type="checkbox" id="agb" class="form-check-input"/>
-                <label for="agb" class="form-check-label">ABG und Datenschutzbedingungen akzeptieren</label>
-            </div>
             <button id="btnsubmit" type="submit" class="btn btn-primary">Einloggen</button>
-            <p>Noch nicht registriert? <a href="registrierung">Registrierung</a></p>
-            <p>Da der Login-Vorgang technisch noch nicht realisiert wurde: <a href="todos">Überspringen</a></p>
             </form>
         </div>
         <div class="col-2"></div>
