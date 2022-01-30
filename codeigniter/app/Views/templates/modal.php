d1d54790a72645aad3d4bc6b26a73c0fb25d5512
nilslambertz <nilslambertz@web.de>
2022-01-30 01:02:11 +0100

    add modal for delete

---
+++ codeigniter/app/Views/templates/modal.php
@@ -0,0 +1,16 @@
+<div class="modal fade" id="confirmDeleteModal" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Löschen?</h5>
+            </div>
+            <div class="modal-body">
+                <p>Soll dieser Eintrag wirklich gelöscht werden?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" data-dismiss="modal" class="btn btn-primary" id="cancel">Abbrechen</button>
+                <button type="button" data-dismiss="modal" class="btn btn-danger" id="delete">Löschen</button>
+            </div>
+        </div>
+    </div>
+</div>
